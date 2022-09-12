f1867fc0c1594a8f5b46a7e11ffb3424e97c3e53
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-09-12 23:30:28 +0300

    added related_names for evolutions

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,56 +3,67 @@ from django.utils import timezone
 
 
 class Pokemon(models.Model):
-    title = models.TextField(default="", verbose_name="Название покемона")
-    title_en = models.TextField(
-        blank=True, verbose_name="Название покемона(англ.)"
-    )
-    title_jp = models.TextField(
-        blank=True, verbose_name="Название покемона(яп.)"
-    )
-    photo = models.ImageField(
-        upload_to="media", blank=False, verbose_name="Фото покемона"
-    )
-    description = models.TextField(
-        blank=False, verbose_name="Описание покемона"
-    )
+    title = models.CharField(max_length=200, verbose_name="Название покемона")
+    title_en = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name="Название покемона(англ.)"
+                                )
+    title_jp = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name="Название покемона(яп.)"
+                                )
+    photo = models.ImageField(upload_to="media/",
+                              blank=True,
+                              verbose_name="Фото покемона"
+                              )
+    description = models.TextField(blank=True,
+                                   verbose_name="Описание покемона"
+                                    )
     img_url = models.CharField(
         max_length=200,
         null=True,
         blank=True,
         verbose_name="Ссылка на фото покемона",
-    )
-    evolution = models.ForeignKey(
-        "Pokemon",
+        )
+    descendant = models.ForeignKey(
+        'self',
         on_delete=models.PROTECT,
         null=True,
         blank=True,
-        related_name="pokemon_evolution",
+        related_name="evolutions",
+        related_query_name="evolution",
         verbose_name="Эволюция покемона",
-    )
+        )
 
     def __str__(self):
-        return "{}".format(self.title)
+        return self.title
 
 
 class PokemonEntity(models.Model):
     latitude = models.FloatField(verbose_name="Широта")
     longtitude = models.FloatField(verbose_name="Долгота")
     pokemon = models.ForeignKey(
-        Pokemon, on_delete=models.CASCADE, verbose_name="Покемон"
-    )
+                                Pokemon,
+                                on_delete=models.CASCADE,
+                                verbose_name="Покемон",
+                                related_name='pokemon_name'
+                                 )
     appeared_at = models.DateTimeField(
-        default=timezone.now, verbose_name="Время появления"
-    )
+                                       default=timezone.now,
+                                       verbose_name="Время появления"
+                                      )
     disappeared_at = models.DateTimeField(
-        default=timezone.now, verbose_name="Время исчезновения"
-    )
-    level = models.IntegerField(blank=True, default=0, verbose_name="Уровень")
-    health = models.IntegerField(
-        blank=True, default=0, verbose_name="Здоровье"
-    )
-    strenght = models.IntegerField(blank=True, default=0, verbose_name="Сила")
-    defence = models.IntegerField(blank=True, default=0, verbose_name="Защита")
-    stamina = models.IntegerField(
-        blank=True, default=0, verbose_name="Выносливость"
-    )
+                                          default=timezone.now,
+                                          verbose_name="Время исчезновения"
+                                          )
+    level = models.IntegerField(blank=True, verbose_name="Уровень")
+    health = models.IntegerField(null=True,
+                                 blank=True,
+                                 verbose_name="Здоровье"
+                                 )
+    strength = models.IntegerField(null=True, blank=True, verbose_name="Сила")
+    defence = models.IntegerField(null=True, blank=True, verbose_name="Защита")
+    stamina = models.IntegerField(null=True,
+                                  blank=True,
+                                  verbose_name="Выносливость"
+                                  )
